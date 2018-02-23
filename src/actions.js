9fdbe6634aa6bed5efb953fdcca484fb80281f95
Mel hsieh <mehsieh89@gmail.com>
2018-02-23 00:36:07 -0800

    removed react icon in tab, removed readme

--- src/actions.js
+++ src/actions.js
@@ -1,10 +1,3 @@
-// export const changeName = (text) => {
-//   return {
-//     type: 'CHANGE_NAME',
-//     payload: text
-//   };
-// };
-
 export const toggleAboutDialog = () => {
   return {
     type: 'TOGGLE_ABOUT_DIALOG',
