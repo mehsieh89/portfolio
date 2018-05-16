460f205ac0e4d9845d7bbec4f4321121e942a80b
Mel hsieh <mehsieh89@gmail.com>
2018-05-15 22:46:43 -0700

    react-slick method broken, cleaned up some code

--- src/reducers/headerReducer.js
+++
@@ -1,8 +0,0 @@
-export default function(state = 'Mel Hsieh', action) {
-  switch (action.type) {
-  case 'CHANGE_NAME':
-    return action.payload;
-  default:
-    return state;
-  }
-}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
-import headerReducer from './headerReducer.js';
 import dialog from './dialogReducer.js';
 
 const allReducers = combineReducers({
-  name: headerReducer,
   dialog: dialog,
   // createEventForm: createEventFormReducer,
 });
