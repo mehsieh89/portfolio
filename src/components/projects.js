937fdbfb4e1e3c2eb1f2a5fcc1c990b4bf1826f7
Mel hsieh <mehsieh89@gmail.com>
2018-06-20 17:17:25 -0700

    added notes and in transition for projects drop down menu

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,17 +1,52 @@
 import React, { Component } from 'react';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+//add objects for each project with their criteria
+//import actions from above
+//activate dialog on click
+//style menu items with hover if possible
+//adjust slide to correspond with hover
 
 class Projects extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      open: false
+    }
   }
 
-  handleOnClick() {
+  handleOnClick = () => {
+    this.setState({
+      open: true
+    })
+  }
 
+  handleClose = () => {
+    this.setState({
+      open: false
+    })
   }
 
   render() {
     return (
-      <div className="project animated fadeIn projectsButton" onClick={this.handleOnClick.bind(this)}> projects </div>
+      <div>
+        <div className="project animated fadeIn projectsButton"
+          onClick={this.handleOnClick}>
+          projects
+        </div>
+        <Menu
+          id="long-menu"
+          open={this.state.open}
+          onClose={this.handleClose}
+          PaperProps={{
+            style: {
+              width: 200,
+            },
+          }}
+        >
+        </Menu>
+      </div>
     );
   }
 }
